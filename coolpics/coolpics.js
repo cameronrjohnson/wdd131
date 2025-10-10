fcc49ca30436acd1fc6d53eaefa47131aee9518f
Cameron Johnson <cajoro935@gmail.com>
2025-10-10 13:01:26 -0600

    Added click on photo functionality

--- coolpics/coolpics.js
+++ coolpics/coolpics.js
@@ -1,11 +1,49 @@
+// Elements
+const gallery = document.querySelector('.gallery');
+const modal = document.querySelector('dialog');
+const modalImage = modal.querySelector('img');
+const closeBtn = modal.querySelector('.close-viewer');
+
+// Navigation
 document.addEventListener('DOMContentLoaded', function () {
 	const btn = document.querySelector('.menu-toggle');
 	const nav = document.getElementById('main-nav');
 
-	if (!btn || !nav) return;
-
 	btn.addEventListener('click', function () {
 		const isOpen = nav.classList.toggle('open');
 		btn.setAttribute('aria-expanded', String(isOpen));
+	});
+
+// Resize Function
+function handleResize() {
+	const menu = document.querySelector('#main-nav');
+	if (window.innerWidth <= 1000) {
+		menu.classList.remove('open');
+	} else {
+		menu.classList.add('open');
+	}
+}
+
+handleResize();
+window.addEventListener('resize', handleResize);
+
+gallery.addEventListener('click', (event) => {
+	const img = event.target.closest('img');
+	if (!img) return;
+
+	let src = img.getAttribute('src');
+	let alt = img.getAttribute('alt');
+
+	if (src.includes('-sm')) {
+		src = src.split('-')[0] + '-full.jpeg';
+	}
+
+	modalImage.src = src;
+	modalImage.alt = alt;
+	modal.showModal();
+});
+
+closeBtn.addEventListener('click', () => {
+		modal.close();
 	});
 });
